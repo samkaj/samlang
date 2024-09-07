3ec182ec06c5dd050324b7746119de792bcb408c
samkaj <kajavasamuel@gmail.com>
2024-09-07 22:57:11 +0200

    compiler: read file instead of stdin directly

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,17 @@ pub mod common;
 pub mod tokenizer;
 
 fn main() {
+    // read file from command line
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() != 2 {
+        eprintln!("Usage: {} <filename>", args[0]);
+        std::process::exit(1);
+    }
+
+    let filename = &args[1];
+    let source = std::fs::read_to_string(filename).expect("Failed to read file");
+
     let mut tokenizer = tokenizer::Tokenizer::new();
-    let mut input = String::new();
-    std::io::stdin().read_line(&mut input).unwrap();
-    let tokens = tokenizer.tokenize(&input);
-    tokenizer.print_tokens(&tokens.unwrap());
+    let tokens = tokenizer.tokenize(&source).unwrap();
+    tokenizer.print_tokens(&tokens);
 }
